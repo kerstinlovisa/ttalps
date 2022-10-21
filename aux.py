1d91a00c63d9a023e6f82eb8f6a5c8edf9111836
Ruth Schäfer <r3schaefer@gmail.com>
2022-10-21 17:27:55 +0000

    Add documentation to aux.py

--- aux.py
+++ aux.py
@@ -1,10 +1,12 @@
 import os
 import sys
 
+# Defines input and putput paths to use in the rest of the project
 COMMON_OUTPUT_PATH = "/home/hd/hd_hd/hd_cu194/ttalps/Output/" # "/home/ruth/Documents/Plots/"
 COMMON_INPUT_PATH = "/home/hd/hd_hd/hd_cu194/Samples/ttalps/" #"/home/ruth/Documents/samples/"
 
 class HiddenPrints:
+    """with HiddenPrints(): hides all stdout within this environment"""
     def __enter__(self):
         self._original_stdout = sys.stdout
         sys.stdout = open(os.devnull, 'w')
