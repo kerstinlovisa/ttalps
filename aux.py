71f7dd74b5f77ba82981e4d466b4e146933672f7
Lovisa Rygaard <kerstinlovisa@github.com>
2023-01-13 14:51:50 +0100

    More clean-up

--- aux.py
+++ aux.py
@@ -2,10 +2,11 @@ import os
 import sys
 
 # Defines input and putput paths to use in the rest of the project
-COMMON_OUTPUT_PATH = '/eos/user/l/lrygaard/ttalp/output/' #"/home/hd/hd_hd/hd_cu194/ttalps/Output/" # "/home/ruth/Documents/Plots/"
+COMMON_OUTPUT_PATH = '/afs/cern.ch/user/l/lrygaard/private/ALPpheno/ttalps/output/' #"/home/hd/hd_hd/hd_cu194/ttalps/Output/" # "/home/ruth/Documents/Plots/"
 COMMON_INPUT_PATH = '/eos/user/j/jalimena/TTALP/lhe_files/' # "/home/hd/hd_hd/hd_cu194/Samples/ttalps/" #"/home/ruth/Documents/samples/"
 TDALPS_PATH = '/afs/cern.ch/user/l/lrygaard/private/ALPpheno/TdAlps/'
-MUON_DATA_PATH = '/afs/cern.ch/user/l/lrygaard/private/madanalysis5/ALPanalysis/Output/TXT/ttj100000/'
+# MUON_DATA_PATH = '/afs/cern.ch/user/l/lrygaard/private/madanalysis5/ALPanalysis/Output/TXT/ttj100000/'
+MUON_DATA_PATH = '/eos/user/l/lrygaard/ttalp/ttj_cluster_runs/'
 
 class HiddenPrints:
     """with HiddenPrints(): hides all stdout within this environment"""
