ba75d0847f5577561a0fd6611de5523451b54ece
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2023-04-21 09:04:41 +0200

    Updating condor scripts

--- madgraph_production_tools/run_production.sh
+++ madgraph_production_tools/run_production.sh
@@ -6,8 +6,12 @@ firstletter=${username:0:1}
 if [ $username == "jniedzie" ]
   then
   user_base_path="/afs/desy.de/user/${firstletter}/${username}/ttalps/ttalps/madgraph_production_tools/"
-  output_path="/nfs/dust/cms/user/${username}/ttalps/"
   python_path="/afs/desy.de/user/${firstletter}/${username}/miniconda3/envs/tta/bin/python3"
+
+  #output_path="/nfs/dust/cms/user/jniedzie/ttalps"
+  #output_path="/nfs/dust/cms/user/jniedzie/ttalps/signals_ctau-1mm"
+  #output_path="/nfs/dust/cms/user/jniedzie/ttalps/signals_ctau-1e2mm"
+  output_path="/nfs/dust/cms/user/jniedzie/ttalps/signals_ctau-1em4mm"
 
   export PYTHIA8DATA=/afs/desy.de/user/${firstletter}/${username}/MG5_aMC_v3_4_2/HEPTools/pythia8/share/Pythia8/xmldoc
 else
